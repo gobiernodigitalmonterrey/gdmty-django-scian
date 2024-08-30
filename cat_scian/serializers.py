d1a34471064ae86a1bbdbedf54d073dab7793adc
César Benjamín <mathereall@gmail.com>
2024-08-29 18:55:42 -0600

    cat_scian

--- cat_scian/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import SCIAN
-
-class SCIANSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = SCIAN
-        fields = '__all__'
