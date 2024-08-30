5b39685ce214d51580baddfc10d6f2b088451c8c
César Benjamín <mathereall@gmail.com>
2024-08-29 18:13:58 -0600

    cat_scian

--- cat_scian/apps.py
+++ cat_scian/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-class ScianConfig(AppConfig):
+class GDMTYCatScianConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gdmty_cat_scian'
     label = 'gdmty_cat_scian'
